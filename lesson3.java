54c72b1c6b7e128bad3504013afe83c838e015d0
DioLloyd <33008255+DioLloyd@users.noreply.github.com>
2020-11-12 20:01:43 +0300

    Update and rename MainFile to lesson3.java

---
+++ lesson3.java
@@ -0,0 +1,48 @@
+package ru.geekbrains.lesson3;
+
+
+import java.util.Scanner;
+
+public class lesson3 {
+    private static String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
+
+    public static void main(String[] args) {
+        String word = random();
+        System.out.println(word);
+        System.out.println("Угадай продукт:");
+
+        Scanner scannerWord = new Scanner(System.in);
+        String inputWord = scannerWord.nextLine();
+
+        do {
+            wrongAnswer(word, inputWord);
+            System.out.println("Попробуйте ещё:");
+            inputWord = scannerWord.nextLine();
+        } while (!inputWord.equals(word));
+        System.out.println("Правильно! Вы выиграли!");
+    }
+
+    private static void wrongAnswer(String word, String inputWord) {
+        int minVal = Math.min(word.length(), inputWord.length());
+        StringBuilder c = new StringBuilder();
+        for (int i = 0; i < minVal; i++) {
+            char a = word.charAt(i);
+            char b = inputWord.charAt(i);
+            if (a == b) {
+                c.append(a);
+            } else {
+                c.append("#");
+            }
+        }
+        for (int j = 0; j < (15 - minVal); j++) {
+            c.append("#");
+        }
+        System.out.println(c);
+    }
+
+    public static String random() {
+        int rand = (int) (Math.random() * words.length);
+        return words[rand];
+    }
+
+}
