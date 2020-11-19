0884acf3df492bdf84590f2b12d72abc4b47d291
Karl <dio.lloyd@gmail.com>
2020-11-19 05:07:43 +0300

    add smart AI

--- src/main/java/lesson4/Lesson4.java
+++ src/main/java/lesson4/Lesson4.java
@@ -5,35 +5,39 @@ import java.util.Scanner;
 public class Lesson4 {
 
     private static final int FIELD_SIZE = 3;
-    private static final char[][] FIELD = new char[FIELD_SIZE][FIELD_SIZE];
+    static char[][] FIELD = new char[FIELD_SIZE][FIELD_SIZE];
     private static final Scanner SCANNER = new Scanner(System.in);
-    private static final char X_VALUE = 'X';
-    private static final char O_VALUE = 'O';
-    private static final char EMPTY_VALUE = '.';
-    private static int moveX;
-    private static int moveY;
+    static final char X_VALUE = 'X';
+    static final char O_VALUE = 'O';
+    static final char EMPTY_VALUE = '.';
+    static int moveX;
+    static int moveY;
     private static int winner = 0;
 
-
     public static void main(String[] args) {
+        game(false, false);
+    }
+
+    private static void game(boolean player1, boolean player2) {
         fillField();
         printField();
-
         do {
             do {
-//                playerTurn();
-                aiTurn();
+                if (player1) playerTurn();
+                else aiTurn(X_VALUE, O_VALUE);
             } while (!turnDrawnX());
             printField();
-
             if (!checkWin()) {
                 do {
-                    aiTurn();
+                    if (player2) playerTurn();
+                    else aiTurn(O_VALUE, X_VALUE);
                 } while (!turnDrawnO());
                 printField();
             }
         } while (!checkWin());
-        System.out.println("Game over\n"+ (winner == 1 ? "player won" : winner == -1 ? "AI won" : "draw"));
+        String player1Name = player1 ? "Игрок 1" : "Компьютер 1";
+        String player2Name = player2 ? "Игрок 2" : "Компьютер 2";
+        System.out.println("Game over\n" + (winner == 1 ? "Победил " + player1Name : winner == -1 ? "Победил " + player2Name : "Ничья"));
     }
 
     public static void fillField() {
@@ -66,7 +70,7 @@ public class Lesson4 {
     }
 
     public static boolean turnDrawnX() {
-        if (check()) {
+        if (checkEmpty()) {
             FIELD[moveY - 1][moveX - 1] = X_VALUE;
             return true;
         } else {
@@ -74,12 +78,12 @@ public class Lesson4 {
         }
     }
 
-    public static boolean check() {
+    public static boolean checkEmpty() {
         return FIELD[moveY - 1][moveX - 1] == EMPTY_VALUE;
     }
 
     public static boolean turnDrawnO() {
-        if (check()) {
+        if (checkEmpty()) {
             FIELD[moveY - 1][moveX - 1] = O_VALUE;
             return true;
         } else {
@@ -87,22 +91,101 @@ public class Lesson4 {
         }
     }
 
-    public static void aiTurn() {
-        int aiTurn = (int) (Math.random() * 3 + 1);
-        moveX = aiTurn;
-        aiTurn = (int) (Math.random() * 3 + 1);
-        moveY = aiTurn;
+    public static void aiTurnRandom() {
+        do {
+            int aiTurn = (int) (Math.random() * 3 + 1);
+            moveX = aiTurn;
+            aiTurn = (int) (Math.random() * 3 + 1);
+            moveY = aiTurn;
+        } while (!checkEmpty());
+        System.out.printf("Хожу рандомно в %d %d%n", moveX, moveY);
     }
 
-    public static void aiTurnSmart() {
+    public static boolean aiTurnSmart(char enemy, boolean againstMyself) {
+        int qRow, qCol;
+        boolean hasEmptyRow, hasEmptyCol;
+        int emptyRowX, emptyRowY;
+        int emptyColX, emptyColY;
 
-        //чекнуть как винчеке и посмотреть есть ли там два крестика и пустая клетка
-        //Если нет хода который надо блокировать, то рандомь
+        int qDiag1 = 0, qDiag2 = 0;
+        boolean hasEmptyDiag1 = false, hasEmptyDiag2 = false;
+        int emptyDiag1X = 0, emptyDiag1Y = 0;
+        int emptyDiag2X = 0, emptyDiag2Y = 0;
 
-        int aiTurn = (int) (Math.random() * 3 + 1);
-        moveX = aiTurn;
-        aiTurn = (int) (Math.random() * 3 + 1);
-        moveY = aiTurn;
+        for (int i = 0; i < FIELD.length; i++) {
+            qRow = 0;
+            qCol = 0;
+            emptyRowX = 0;
+            emptyRowY = 0;
+            emptyColX = 0;
+            emptyColY = 0;
+            hasEmptyRow = false;
+            hasEmptyCol = false;
+            for (int j = 0; j < FIELD.length; j++) {
+                if (FIELD[i][j] == EMPTY_VALUE) {
+                    hasEmptyRow = true;
+                    emptyRowX = j;
+                    emptyRowY = i;
+                }
+                if (FIELD[i][j] == enemy) {
+                    qRow = qRow + 1;
+                }
+
+                if (FIELD[j][i] == EMPTY_VALUE) {
+                    hasEmptyCol = true;
+                    emptyColX = i;
+                    emptyColY = j;
+                }
+                if (FIELD[j][i] == enemy) {
+                    qCol = qCol + 1;
+                }
+                if (qCol == 2 && hasEmptyCol || qRow == 2 && hasEmptyRow) {
+                    moveX = (qCol == 2 && hasEmptyCol ? emptyColX : emptyRowX) + 1;
+                    moveY = (qCol == 2 && hasEmptyCol ? emptyColY : emptyRowY) + 1;
+                    if (!againstMyself) {
+                        System.out.print("Вижу, что нужно блокировать " + (qCol == 2 ? "столбец " : "строку ") + (i + 1));
+                    } else {
+                        System.out.print("Вижу, что нужно выигрывать " + (qCol == 2 ? "столбец " : "строку ") + (i + 1));
+                    }
+                    System.out.printf(". Хожу %d %d%n", moveX, moveY);
+                    return true;
+                }
+            }
+            if (FIELD[i][i] == EMPTY_VALUE) {
+                hasEmptyDiag1 = true;
+                emptyDiag1X = i;
+                emptyDiag1Y = i;
+            }
+            if (FIELD[i][i] == enemy) {
+                qDiag1 = qDiag1 + 1;
+            }
+
+            if (FIELD[i][FIELD.length - 1 - i] == EMPTY_VALUE) {
+                hasEmptyDiag2 = true;
+                emptyDiag2X = FIELD.length - 1 - i;
+                emptyDiag2Y = i;
+            }
+            if (FIELD[i][FIELD.length - 1 - i] == enemy) {
+                qDiag2 = qDiag2 + 1;
+            }
+
+            if (qDiag1 == 2 && hasEmptyDiag1 || qDiag2 == 2 && hasEmptyDiag2) {
+                moveX = (qDiag1 == 2 && hasEmptyDiag1 ? emptyDiag1X : emptyDiag2X) + 1;
+                moveY = (qDiag1 == 2 && hasEmptyDiag1 ? emptyDiag1Y : emptyDiag2Y) + 1;
+                if (!againstMyself) {
+                    System.out.print("Вижу, что нужно блокировать " + (qDiag1 == 2 ? "левую диагональ" : "правую диаганоль"));
+                } else {
+                    System.out.print("Вижу, что нужно выигрывать " + (qDiag1 == 2 ? "левую диагональ" : "правую диаганоль"));
+                }
+                System.out.printf(". Хожу %d %d%n", moveX, moveY);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void aiTurn(char me, char enemy) {
+        if (!aiTurnSmart(me, true) && !aiTurnSmart(enemy, false)) aiTurnRandom();
     }
 
     public static boolean checkWin() {
@@ -143,7 +226,7 @@ public class Lesson4 {
             } else if (FIELD[i][FIELD.length - 1 - i] == O_VALUE) {
                 oDiag2 = oDiag2 + 1;
             }
-            if (xDiag1 == 3 || oDiag1 == 3) {
+            if (xDiag1 == 3 || xDiag2 == 3 || oDiag1 == 3 || oDiag2 == 3) {
                 winner = xDiag1 == 3 || xDiag2 == 3 ? +1 : -1;
                 return true;
             }
